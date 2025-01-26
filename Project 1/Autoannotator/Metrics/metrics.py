7bbbeb4071619a6319abb0dc91f214ce52581e67
Bence Lukacsy <bence.lukacsy@gmail.com>
2025-01-26 12:31:55 -0500

    made easier to test different model types

--- Project 1/Autoannotator/Metrics/metrics.py
+++ Project 1/Autoannotator/Metrics/metrics.py
@@ -4,15 +4,21 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import sklearn.metrics as metrics
 
-test_features = np.load('../../Arrays/test_features.npy')
-test_labels = np.load('../../Arrays/test_labels.npy')
+name = "airway_epithelium"
 
-model = keras.models.load_model("../../Models/granulomas_final_tf_nn_v3.h5") 
+test_features = np.load(f"../../Arrays/test_features_{name}.npy")
+test_labels = np.load(f"../../Arrays/test_labels_{name}.npy")
+
+binary = False
+if len(np.unique(test_labels)) == 2: binary = True
+
+model = keras.models.load_model(f"../../Models/granulomas_final_tf_nn_{name}_v1.h5") 
 
 print(model.summary())
 
 prediction = model.predict(test_features)
 max_indices = np.argmax(prediction, axis=1)
+if binary: prediction = prediction[:, 1]
 
 def overall_metrics(y_true, y_pred, average='weighted'):
 
@@ -34,8 +40,17 @@ def create_confusion_matrix(y_true, y_pred):
 def plot_confusion_matrix(y_true, y_pred):
     confusion_matrix = create_confusion_matrix(y_true, y_pred)
 
+    if binary:
+        print('\nconfusion matrix:')
+        print('true negatives:', confusion_matrix[0][0])
+        print('false positives:', confusion_matrix[0][1])
+        print('false negatives:', confusion_matrix[1][0])
+        print('true positives:', confusion_matrix[1][1])
+        print('total class 0:', np.sum(confusion_matrix[0]))
+        print('total class 1:', np.sum(confusion_matrix[1]))
+
     plt.figure(figsize=(8, 6))
-    sb.heatmap(confusion_matrix, annot=False, cmap='Reds', cbar=True, xticklabels=np.unique(np.arange(30)), yticklabels=np.unique(np.arange(30)))
+    sb.heatmap(confusion_matrix, annot=True, fmt="d", cmap='Reds', cbar=True, xticklabels=np.unique(test_labels), yticklabels=np.unique(test_labels))
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
@@ -58,5 +73,5 @@ print("class metrics:", class_metrics(test_labels, max_indices))
 print("roc_auc ovr:", roc_auc_ovr(test_labels, prediction))
 print("roc_auc ovo:", roc_auc_ovo(test_labels, prediction))
 print("average precision:", average_precision(test_labels, prediction))
-print("balanced accuracy:",balanced_accuracy(test_labels, max_indices))
+print("balanced accuracy:", balanced_accuracy(test_labels, max_indices))
 plot_confusion_matrix(test_labels, max_indices)
