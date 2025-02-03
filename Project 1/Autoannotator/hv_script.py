49805b6ff337ea7df95d54d188b24bee1d7b306b
Bence Lukacsy <bence.lukacsy@gmail.com>
2025-02-02 23:37:54 -0500

    files deleted

--- Project 1/Autoannotator/hv_script.py
+++
@@ -1,6 +0,0 @@
-# Subsetting
-adata_hv = adata[:, adata.var['highly_variable'] ].copy()
-
-# Showing dimension and how many genes are hv just to know
-print("No. of highly variable genes:", adata.var['highly_variable'].sum())
-print("Shape:", adata_hv.shape)
