5e0081c729229b4be0678afb26ad5275f6cf025e
Bence Lukacsy <bence.lukacsy@gmail.com>
2025-05-28 18:23:33 -0400

    inc dropout

--- Project 1/Autoannotator/Training Scripts/jax_gpu.py
+++ Project 1/Autoannotator/Training Scripts/jax_gpu.py
@@ -45,7 +45,7 @@ def create_model(input_size, num_classes, hidden_layers, dropout_rate, learning_
 input_size = train_features.shape[1]
 num_classes = len(np.unique(train_labels))
 hidden_layers = [2400, 60] # hvg: [300]
-dropout_rate = 0.6 # hvg: 0.85
+dropout_rate = 0.8 # hvg: 0.85
 learning_rate = 1e-4
 weight_decay = 1e-4 # hvg: 1e-2
 epochs = 1000
